adbe259bc82fbe57024ac4dac3f9aa83843162b2
bilevi <89856529+bilevi@users.noreply.github.com>
2022-09-20 14:11:52 -0400

    Update sbgui_convert.py
    Updated contains() in accordance to python - not java

--- pythonGUI/sbgui_convert.py
+++ pythonGUI/sbgui_convert.py
@@ -13,13 +13,14 @@ class convert:
     X_Coord = 0
     Y_Coord = 0
     Z_Coord = 0
+    numPattern = re.compile("[0-9]+[.]")
     
     def __init__(self, fileName) :
         self.fileName = fileName
 
     
     def readInFile(self) :
-        isFlowing = false
+        isFlowing = False
         
         
         # If the file isn't GCODE, display error and break out of method
@@ -33,42 +34,44 @@ class convert:
         with GCodeFile as scan :
             line = GCodeFile.readline()
             while line :
-                if [(line.contains('E') and re.search(r'\d', line)) and isFlowing == false] :
+                if [(line.__contains__("E" + str(numPattern))) and isFlowing == False] :
+                    isFlowing = True
                     SBPFile.writeline("S0, 1, 1")                   
-                if [not (line.contains('E') and re.search(r'\d', line)) and isFlowing == true] :
+                if [not (line.__contains__("E" + str(numPattern))) and isFlowing == True] :
+                    isFlowing = False
                     SBPFile.writeline("S0, 1, 0")
                
-                if line.contains('G0') :
+                if line.__contains__('G0') :
                     self.getCoord(line)
-                if line.contains('G1') :
+                if line.__contains__('G1') :
                     self.getCoord(line)
                     
         SBPFile.close()        
 
                     
     def getCoords(self, line) :
-        if line.contains('X' + re.search(r'\d', line) + ' ') :
+        if line.__contains__("X" + str(numPattern) + " ") :
             self.getNums(line, 'X', ' ')
-        elif line.contains('X' + re.search(r'\d', line) + '\n') :
+        elif line.contains("X" + str(NumPattern) + "\n") :
             self.getNums(line, 'X', '\n')
                     
-        if line.contains('Y' + re.search(r'\d', line) + ' ') :
+        if line.__contains__("Y" + str(NumPattern) + " ") :
             self.getNums(line, 'Y', ' ')
-        elif line.contains('Y' + re.search(r'\d', line) + '\n') :
+        elif line.__contains__('Y' + str(NumPattern) + "\n") :
             self.getNums(line, 'Y', '\n')
                     
-        if line.contains('Z' + re.search(r'\d', line) + ' ') :
+        if line.__contains__("Z" + str(NumPattern) + " ") :
             self.getNums(line, 'Z', ' ')   
-        elif line.contains('Z' + re.search(r'\d', line) + '\n') :
+        elif line.__contains__("Z" + str(NumPattern) + "\n") :
             self.getNums(line, 'Z', '\n')
                     
     def getNums(self, line, char, space) :
         if char == 'X' :
-            self.X_Coord = float(line[line.find(char)+1 : line.find(space)]
+            X_Coord = float(line[line.find(char)+1 : line.find(space)])
         if char == 'Y' :
-            self.Y_Coord = float(line[line.find(char)+1 : line.find(space)]
+            Y_Coord = float(line[line.find(char)+1 : line.find(space)])
         if char == 'Z' :
-            self.Z_Coord = float(line[line.find(char)+1 : line.find(space)]
+            Z_Coord = float(line[line.find(char)+1 : line.find(space)])
                             
    
     
